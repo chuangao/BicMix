0366ffa813f467ba104c1f44475af26b7f2ef1c9
chuangao <chuan.gao.cornell@gmail.com>
2020-10-30 07:36:33 -0400

    manually deleted files

--- IFA/chuan_script.m
+++
@@ -1,5 +0,0 @@
-[LAM,EX]=IFA_chuan(file,kinit);
-
-
-dlmwrite(strcat('Lambda.txt'), LAM, 'delimiter', '\t','precision', 6);
-dlmwrite(strcat('EX.txt'), EX, 'delimiter', '\t','precision', 6);
--- IFA/readme.m
+++
@@ -1,3 +0,0 @@
-% gendat.m  generates simulated data
-
-% spfactcovest_mgploadings.m implements the Gibbs sampler in bka paper
